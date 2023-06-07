eb006b5033c53b9647639a483cdd93126fa7f9f5
MutedAmbitions <83297128+MutedAmbitions@users.noreply.github.com>
2023-06-07 13:38:13 -0400

    Uploaded "First.c"

---
+++ C Scripts/First.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main (){
+    printf("Hello World!\n");
+    sleep(3);
+    printf("\aYou should get a damn alert\a.\n");
+    sleep(0.95);
+    printf("\aYou should get a damn alert\a.\n");
+    sleep(0.95);
+    printf("\aYou should get a damn alert\a.\n");
+    sleep(0.95);
+    printf("\aYou should get a damn alert\a.\n");
+    sleep(0.95);
+    printf("\aYou should get a damn alert\a.\n");
+    sleep(0.95);
+    printf("\aYou should get a damn alert\a.\n");
+    sleep(0.95);
+    printf("\aYou should get a damn alert\a.\n");
+    sleep(0.95);
+    printf("\aYou should get a damn alert\a.\n");
+    sleep(0.95);
+    printf("\aYou should get a damn alert\a.\n");
+    float value = 3.145678;
+    printf("%.3f\n", value);
+    return 0;
+}
